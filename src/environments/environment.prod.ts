1e11350dff3af353ec06daef48c6229e362fa57f
pktherock <pktherock00@gmail.com>
2023-11-15 18:31:07 +0530

    confgure env variables for all environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   name: 'prod',
-  googleApiKey: process.env['GOOGLE_API_KEY'],
-  auth0ClientId: process.env['AUTH0_CLIENT_ID'],
+  googleApiKey: import.meta.env['NG_APP_GOOGLE_API_KEY'],
+  auth0ClientId: import.meta.env['NG_APP_AUTH0_CLIENT_ID'],
 };
